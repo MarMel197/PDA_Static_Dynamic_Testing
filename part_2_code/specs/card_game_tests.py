3429cef9cd85e88f238033b38d7be58efbdfa7e7
Mark <markmeldrum70@gmail.com>
2021-01-31 20:53:57 +0000

    Checks for highest card

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -15,3 +15,11 @@ class TestCardGame(unittest.TestCase):
 
     def test_check_for_ace__False(self):
         self.assertEqual(False, CardGame.check_for_ace(self, self.card2))
+
+    def test_check_highest_card(self):
+        self.assertEqual(self.card2, CardGame.highest_card(self, self.card1, self.card2))
+
+    def test_check_highest_card__swapped(self):
+        self.card1 = Card("clubs", 5)
+        self.card2 = Card("hearts", 1)
+        self.assertEqual(self.card1, CardGame.highest_card(self, self.card1, self.card2))
